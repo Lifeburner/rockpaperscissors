0de678a129c6f37dffe85956a5e20189c90f40e2
Truerror <uitiong@live.com>
2021-07-25 10:17:24 +0800

    rpsRound done

--- index.js
+++ index.js
@@ -6,5 +6,21 @@ function computerPlay () {
 }
 
 function rpsRound (playerSelection, computerSelection) {
-    //pass
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+    if (player === "rock") {
+        if (computer === "rock") { return "tied"; }
+        else if (computer === "paper") { return "computer wins"; }
+        else { return "you win"; }
+    }
+    if (player === "paper") {
+        if (computer === "rock") { return "you win"; }
+        else if (computer === "paper") { return "tied" }
+        else { return "computer wins"; }
+    }
+    if (player === "scissors") {
+        if (computer === "rock") { return "computer wins"; }
+        else if (computer === "paper") { return "you win"; }
+        else {return "tied"; }
+    }
 }
